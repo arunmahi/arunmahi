b1e279ee698429357af1ae734dbe63775216db68
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-01-23 16:25:37 +0530

    Update arun vowel.c

--- arun vowel.c
+++ arun vowel.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-char z,a,e,i,o,u;
+char z;
+printf("given letter z is");
 scanf("%c",&z);
-if(z=='a'||z='e'||z='i'||z='o'||z='u')
+if(z=='a'||z=='e'||z=='i'||z=='o'||z=='u')
 printf("vowel");
 else
 printf("consonant");
-getch();
 }
