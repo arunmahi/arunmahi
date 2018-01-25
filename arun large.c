86e25fdde36fa213bf59e8fe4a9bac48e0167f25
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-01-25 16:32:38 +0530

    Create arun large.c

---
+++ arun large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+{
+printf("a is greater no.");
+}
+else if(b>c)
+{
+printf("b is greater no.");
+}
+else
+{
+printf("c is greater no.");
+}
+}
