0d31e84195f54c7fbb4ba0277c2c8d4b68bd1402
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-01-26 10:20:51 +0000

    Create arun hours.c

---
+++ arun hours.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+printf("Arun-922515114008");
+int a,b,c;
+printf("\n enter the value a ");
+scanf("%d",&a);
+b=(a/60);
+c=a-((a/60)*60);
+printf("\n %d hours",b);
+printf("\n %d minutes",c);
+}
