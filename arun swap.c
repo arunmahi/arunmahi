0a1589939326eb703b9490176d85fb2f19c89824
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-01-26 09:41:43 +0000

    Create arun swap.c

---
+++ arun swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("Arun-922515114008");   
+int a,b,c;
+printf("\n Enter value of a");
+scanf("%d",&a);
+printf("\n Enter value of b");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("After swapping a=%d b=%d",a,b);
+getch();
+}
