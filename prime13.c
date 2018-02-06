6cd043e8fe4c13209488f62515623217b7dfa474
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-02-06 16:58:03 +0530

    Update prime13.c

--- prime13.c
+++ prime13.c
@@ -4,11 +4,11 @@ void main()
 int a,i,x=0;
 printf("enter the value");
 scanf("%d",&a);
-for(i=0;i<=a;i++)
+for(i=1;i<=a;i++)
 if(a%i==0)
 x++;
 if(x==2)
 printf("it is prime");
 else
-printf("it is not prime);
+printf("it is not prime");
 }
