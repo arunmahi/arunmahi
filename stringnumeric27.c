0906fcfdaf180e3886098968cd32f4eade6cb9c7
arunmahi <35652264+arunmahi@users.noreply.github.com>
2018-02-27 16:13:41 +0530

    Update stringnumeric27.c

--- stringnumeric27.c
+++ stringnumeric27.c
@@ -4,3 +4,17 @@ void main()
 char str[20];
 int i,n;
 printf("enter the string");
+scanf("%s",&str);
+n=strlen(str);
+for(i=0;i<n;i++)
+{
+if(str[i]>='0' && str[i]<='9')
+{
+printf("\n YES");
+}
+else
+{
+printf("\n NO");
+}
+}
+}
